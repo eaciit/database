38da3b1fedf0294ea7ac840a1c5aba58678ab0cb
Arief Darmawan <ariefda@hotmail.com>
2015-07-16 03:34:42 +0700

    Revert 8b32647..59fe732
    This rolls back to commit 8b32647c5e5becb67090ea62fc133c7b6b446ece.

--- mongodb/mongodb_test.go
+++ mongodb/mongodb_test.go
@@ -2,30 +2,30 @@ package mongodb
 
 import (
 	"fmt"
-	"github.com/eaciit/database/query"
+	. "github.com/eaciit/database/query"
 	. "github.com/eaciit/toolkit"
 	"testing"
-	"time"
 )
 
 func TestQ(t *testing.T) {
-	q := query.New(new(MgoQuery)).SetStringSign("'").(*MgoQuery)
-	q.Eq("field1", "Arief Darmawan")
-	q.And()
-	q.Eq("dateTrx", time.Now())
-	q.And()
-	q.O()
-	q.Eq("field2", 100)
-	q.Or()
-	q.Eq("field2", 200)
-	q.C()
+	qes := And(Or(
+		And(Eq("username", "ariefdarmawan"), Eq("action", "system")),
+		And(Eq("username", "someuser"), Eq("action", "log"))),
+		Eq("username", "administrator"))
 
-	cmd := M{}
-	e := q.Command(&cmd, &M{})
-
+	c := new(Result)
+	q := new(MgoQuery)
+	e := q.SetQ(q).SetStringSign("\"").Command(c, nil, qes)
 	if e != nil {
-		t.Error("Unable to parse Q " + e.Error())
+		t.Error("Unable to parse Q")
 	} else {
-		fmt.Printf("Parse result: %v \n", cmd)
+		fmt.Printf("Parse result: %v \n", GetJsonString(c.Data))
+	}
+}
+
+func Do(vs ...string) {
+	fmt.Printf("You enter %d parms \n", len(vs))
+	for _, v := range vs {
+		fmt.Println(v)
 	}
 }
