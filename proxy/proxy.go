a94984833527f4dc3d1a932735fca895619fd816
novalagung <caknopal@gmail.com>
2015-09-01 14:39:19 +0700

    - add sql server to proxy

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/eaciit/database/base"
 	"github.com/eaciit/database/mongodb"
+	"github.com/eaciit/database/mssql"
 	"github.com/eaciit/database/mysql"
 	// "github.com/eaciit/database/oracle"
 	err "github.com/eaciit/errorlib"
@@ -19,13 +20,17 @@ func NewConnection(connectionType string, host string, username string, password
 	if connectionType == "mongodb" {
 		c := mongodb.NewConnection(host, username, password, dbname)
 		return c, nil
-		// } else if connectionType == "oracle" {
-		// 	c := oracle.NewConnection(host, username, password, dbname)
-		// 	return c, nil
 	} else if connectionType == "mysql" {
 		c := mysql.NewConnection(host, username, password, dbname)
 		return c, nil
+	} else if connectionType == "mssql" {
+		c := mssql.NewConnection(host, username, password, dbname)
+		return c, nil
 	}
+	// else if connectionType == "oracle" {
+	// 	c := oracle.NewConnection(host, username, password, dbname)
+	// 	return c, nil
+	// }
 
 	e := err.Error(packageName, "", "NewConnection", fmt.Sprintf("Connection type %s is not yet supported", connectionType))
 	return nil, e
